ce8f28f682f1d63c0f0e8cce8ddb83b463931eda
Anurag290405 <141998867+Anurag290405@users.noreply.github.com>
2025-12-20 01:29:01 +0530

    Added Render URL

--- frontend/src/NwConfig.js
+++ frontend/src/NwConfig.js
@@ -1,5 +1,7 @@
-export const API_URL = ["http://localhost:3000" , "https://devcaps.onrender.com"];
-export const Frontend_API_URL = [ "http://localhost:5173" ,"https://devcapsbyanurag.vercel.app"];
+// export const API_URL = "http://localhost:3000";
+export const API_URL = "https://devcaps.onrender.com"
+// export const Frontend_API_URL = "http://localhost:5173";
+export const Frontend_API_URL = "https://devcapsbyanurag.vercel.app";
 
 // DevCaps endpoints
 export const projects = "/api/projects";
